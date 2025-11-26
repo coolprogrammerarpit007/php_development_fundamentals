2627bf6debd1b56516b1956d483fb87391ac45f8
Arpit <arpit.mishra@microprixs.in>
2025-11-26 16:26:47 +0530

    Variables and Operators in PHP

---
+++ index.php
@@ -0,0 +1,66 @@
+<!-- Variables in PHP -->
+
+<?php
+
+    // echo "Hello World!" . "<br>";
+    // echo("Nice to meet you everyone!");
+
+    // Data Types in PHP
+    // String
+    // Integer
+    // float
+    // Boolean
+
+
+    // Variable Creation in PHP
+
+    $txt = 'Hello World';
+    $txt2 = "Nice to meet you everyone!";
+    $x = 5;
+    $y = 10.5;
+
+    // echo $txt .' ' . $x . ' ' . $y . "<br>";
+    // echo $txt2 . '<br>';
+    // echo $txt . ' ' . $txt2 . '<br>';
+
+    // Variable creation in PHP is case-Sensitive
+
+    // $number1 = (int) readline("Enter Number: ");
+    // $number2 = (int) readline("Enter Number: ");
+
+    // $total = $number1 + $number2;
+    // $subtract = $number1 - $number2;
+    // $division = null;
+
+    // if($number2 > 0)
+    // {
+    //     $division = $number1/$number2;
+    // }
+    // $multiply_numbers = $number1 * $number2;
+
+    // echo "Addition of Two Numbers: " . $total . "<br>";
+    // echo "Subtraction of Two Numbers: " . $subtract . "<br>";
+    // echo "Division of Two Numbers: " . $division . "<br>";
+    // echo "Multiplication of Two Numbers: " . $multiply_numbers . "<br>";
+
+    // echo 5%12;  Remainder Operator
+
+    // Exponential Operator
+    // echo 2 ** 4;
+
+
+    # Increment and decrement operators 
+
+    // $a = 5;
+    // echo ++$a . "<br>";
+    // echo $a++ . "<br>";
+    // echo $a;
+
+
+    $a = true;
+    $b = false;
+
+    // echo $a + $b; -> This is called Implicit conversion in PHP
+    
+?>
+
